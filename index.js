8a3198757ed7a3512b0f6adc46a0c4a9b4b12144
0c0de <gamesfreakin@gmail.com>
2018-01-10 16:36:58 +0100

    Something Here

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.get("/:codeURL", (Request, Response) =>{
 
         db.collection("urls").findOne(urlForSearch, (err, resources) => {
             if(err) throw err;
-            console.log("Founded ;) " + resources);
+            console.log("Founded ;) ", resources);
             Response.location(resources.ORIGINAL_URI);
         });
     });
